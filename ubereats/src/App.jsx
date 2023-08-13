7516388b51466fa2ef8005b174a6ac98914d9e0a
Keshav <keshav2552003@gmail.com>
2023-08-13 11:52:31 +0530

    Added navbar and sidenav

--- ubereats/src/App.jsx
+++ ubereats/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   )
 }
